f6a1b43294c0731d9cc13f9a840f415ccb050a54
Joshua Emmons <skia@skia.net>
2014-01-19 12:58:28 -0600

    Vertically centers label. Adds shadows. * equally spacing things with constraints is interesting: http://stackoverflow.com/questions/19108145/using-constraints-to-keep-two-tableviews-the-same-height-the-same-distance-apa/21221113#21221113 * Have to turn off cells’ view-clipping for shadows to work. Performance implications?

--- CollectionDemo/Views/MCMCollectionViewCell.m
+++ CollectionDemo/Views/MCMCollectionViewCell.m
@@ -5,20 +5,32 @@
 #pragma mark - INIT/SETUP
 -(instancetype)initWithFrame:(CGRect)frame{
   if((self = [super initWithFrame:frame])){
-    [self setup];
+    [self setupConstraints];
+    [self setupShadow];
   }
   return self;
 }
 
 
 -(void)awakeFromNib{
-  [self setup];
+  [self setupConstraints];
+  [self setupShadow];
 }
 
 
--(void)setup{
+-(void)setupConstraints{
   NSLayoutConstraint *squareConstraint = [NSLayoutConstraint constraintWithItem:[self imageView] attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:[self imageView] attribute:NSLayoutAttributeHeight multiplier:1.0f constant:0];
   [squareConstraint setPriority:1000];
-  [[self imageView] addConstraint:squareConstraint];
+  [[self imageView] addConstraint:squareConstraint];  
+}
+
+
+-(void)setupShadow{
+  [self setClipsToBounds:NO];
+  [[self layer] setShadowPath:[[UIBezierPath bezierPathWithRect:[self bounds]] CGPath]];
+  [[self layer] setShadowColor:[[UIColor blackColor] CGColor]];
+  [[self layer] setShadowOffset:CGSizeMake(0.0f, 1.0f)];
+  [[self layer] setShadowRadius:2.0f];
+  [[self layer] setShadowOpacity:0.5f];
 }
 @end
